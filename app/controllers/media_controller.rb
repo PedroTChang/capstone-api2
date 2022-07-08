361d9fa1a44e35f9bcc790e2aa4dacd8827de41d
pedrotchang <28745927+pedrotchang@users.noreply.github.com>
2022-07-07 21:59:03 -0700

    adds destroy action for media

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,5 +1,5 @@
 class MediaController < ApplicationController
-  # before_action :authenticate_user
+  before_action :authenticate_admin, except: [:index, :show]
 
   def index
     @media = Medium.all
@@ -9,5 +9,12 @@ class MediaController < ApplicationController
   def show
     @medium = Medium.find_by(id: params[:id])
     render template: "media/show"
+  end
+
+  def destroy
+    medium_id = params[:id]
+    medium = Medium.find_by(id: medium_id)
+    medium.destroy
+    render json: { message: "Medium has been deleted." }
   end
 end
