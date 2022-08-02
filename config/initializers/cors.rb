ac467216b702e51f2f1d4d7251ad9396e35c3d5d
pedrotchang <pedrochang91@gmail.com>
2022-08-02 10:32:40 -0700

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "localhost:8080"
+    origins "localhost:3000", "localhost:8080", "https://mysterious-brook-71579.herokuapp.com/"
 
     resource "*",
       headers: :any,
